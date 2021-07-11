ff95fec2dfc3751b38e0e3696c20023bb1309e90
23Shivani <shivani.gharve.2310@gmail.com>
2021-07-11 14:43:20 +0530

    Added: Temperature Conversion Program

---
+++ src/com/bridgelabz/Temp/TemperatureConversion.java
@@ -0,0 +1,14 @@
+package com.bridgelabz.Temp;
+
+import com.bridgelabz.Utility.Utility;
+
+public class TemperatureConversion {
+
+	public static void main(String[] args) {
+		{
+			System.out.println("Converting Temperature:");
+			Utility.temperatureConversion();
+		}
+	}
+
+}
--- src/com/bridgelabz/Utility/Utility.java
+++ src/com/bridgelabz/Utility/Utility.java
@@ -1,4 +1,6 @@
 package com.bridgelabz.Utility;
+import java.util.Queue;
+import java.util.Scanner;
 
 public class Utility {
 
@@ -11,4 +13,38 @@ public class Utility {
 			int d0 = (date + x + 31 * m0 / 12) % 7;
 			return d0;
 		}
-	}
+	
+		public static void temperatureConversion() {
+			// TODO Auto-generated method stub
+			System.out.println("\nEnter 1 for Fahrenheit to Celsius" + "\nEnter 2 for Celsius to Fahrenheit"
+					+ "\nSomething else to Exit." + "\nYour Option:");
+			Scanner scanner = new Scanner(System.in);
+			int selection = scanner.nextInt();
+			if (selection == 1) {
+				System.out.println("Enter a degree in Fahrenheit:");
+				fahrenheitToCelsius();
+			} else if (selection == 2) {
+				System.out.println("Enter a degree in Celsius:");
+				celsiusToFahrenheit();
+			} else {
+				System.out.println("Invalid");
+			}
+			scanner.close();
+		}
+		private static void celsiusToFahrenheit() {
+			// TODO Auto-generated method stub
+			Scanner scanner = new Scanner(System.in);
+			Double celsius = scanner.nextDouble();
+			System.out.println(celsius + " celsius is " + ((celsius * 9 / 5.0) + 32) + " Fahrenheit");
+			temperatureConversion();
+			scanner.close();
+			}
+		private static void fahrenheitToCelsius() {
+			// TODO Auto-generated method stub
+			Scanner scanner = new Scanner(System.in);
+			Double Fahrenheit = scanner.nextDouble();
+			System.out.println(Fahrenheit + " Fahrenheit is " + ((Fahrenheit - 32) * (5 / 9.0)) + " celsius");
+			temperatureConversion();
+			scanner.close();
+			}
+}
